1e4ede7b30dbb87be46f03f7143e97a308eb5f7d
Immo <immo.schuett@outlook.de>
2022-06-01 18:25:52 +0200

    added first naive implementations

--- examples/BlockSys.jl
+++ examples/BlockSys.jl
@@ -67,5 +67,100 @@ function Base.iterate(B::BlockSystems, st::Array{Vector{Int}})
 end
 Base.IteratorSize(::BlockSystems) = Base.SizeUnknown()
 
-
+function collect_subfields(C::Oscar.GaloisGrp.GaloisCtx{Hecke.qAdicRootCtx})
+  SFList = []
+  K, _ = number_field(C.f)
+  S = Oscar.GaloisGrp.SubfieldLattice(K, C)
+  n = degree(C.f)
+  Rts = roots(C,7)
+  cyc = gap_perm([findfirst(y -> y == x, Rts) for x = map(frobenius,Rts)])
+  BL = []
+  for d in divisors(n)
+    for bls in BlockSystems(n,d)
+      #!isinvar(bls,cyc) || push!(BL,bls) + Herer seems to be a bug. cons. 
+      # A = [[1, 3], [2, 5], [4, 6]], cyc = (1 2)(3 4)(5 6)
+      # this is not inv. but gices nontriv. Field with... Oscar.GaloisGrp.subfield(S, A)
+      push!(BL,bls) 
+    end 
+  end
+  VBS = [] # valid blocksystems
+  TVBS = []# Temp updated VBS
+  SF = Nothing()
+  for bls in BL 
+      !istriv(bls) || continue
+      # pseudotest with VBS
+      SF = Oscar.GaloisGrp.subfield(S, bls)
+      # update SF
+      if typeof(SF) == Tuple{AnticNumberField, NfToNfMor} # i.e not equal nothing
+        if !isinvar(bls,cyc)
+          println(bls)
+        end
+        push!(SFList,SF)
+        push!(TVBS,bls)
+      end
+      #update info
+      # if TVBS != [] end 
+  end 
+  return SFList
 end
+
+function isinvar(bls,cyc)
+  #return true if no contradiction with cyc
+  for bl in bls
+    Set(cyc.(bl)) in Set.(bls) || return false 
+  end 
+  return true 
+end
+function istriv(bls)
+  length(bls)!=1 || return true
+  iszero(length.(bls).-1) ? (return true) : (return false)  
+end
+end
+
+#TODOSTUFF implement Klüners combinatorical choices to directly include the cyc and reduce many many iterations
+
+function all_subsets(value,k,D)
+  # Dictionary with A -> n_a
+  #=
+   TODO later: combinatorical optimization here.
+   this is kind of partition problem 
+   finding all cycles that gives a block combined with k
+   keyset for 
+  =#
+
+  keyset = Set() # keyset ( k | n_a)
+  for key in keys(D)
+    if D[key]%k == 0
+      push!(keyset,key)
+    end
+  end 
+  S = Hecke.subsets(keyset)
+  L = Set()
+  for set in S 
+    if set == Set()
+      continue
+    end
+    if value == sum(D[i] for i in set ) && 
+      push!(L,set)
+    end 
+  end 
+
+  return L 
+end 
+  
+function all_blocksys_naive(C)
+  Rts = roots(C,7)
+  cyc = gap_perm([findfirst(y -> y == x, Rts) for x = map(frobenius,Rts)])
+  l =  sum(cl for (_,cl) in cycle_structure(cyc))
+  # .... 
+  
+end
+
+#Also good idea:
+#
+# find alll pos blocks:
+# bin. Search tree T, 
+# update Orbits (of perm.)
+# iter through T (avoids doubles orbit calc.)
+# 
+#
