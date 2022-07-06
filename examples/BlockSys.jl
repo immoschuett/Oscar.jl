d956f22bd04213a2acbfc55627fe984ae12697c2
Immo <immo.schuett@outlook.de>
2022-07-06 22:18:51 +0200

    added Tree struc. Working on compressed Blocksys Type and new iterator

--- examples/BlockSys.jl
+++ examples/BlockSys.jl
@@ -115,16 +115,95 @@ end
 # End Module
 ################################################################################################################################
 
-#TODOSTUFF implement Klüners combinatorical choices to directly include the cyc and reduce many many iterations
 
-# TODO Tree structure
-function all_subsets(value,k,D,min,checkset)
-  # Dictionary with A -> n_a
+################################################################################################################################
+# basic tree with unlimited children per node, inpush updated paths to all leaves
+# ids are updated during puhses, nodes are stored by id in a Dict
+# acces via node-id. 
+################################################################################################################################
+mutable struct Node
+  parent::Union{Int64,Missing}
+  children::Vector{Int64}
+  nchildren::Int64
+  value::Any   #later d,n and Vector here
+  path::Vector{Int64}  #<- TODO new type path with attributes length and nodeids etc...
+end
+mutable struct Tree  
+  nodes::Dict{Int64, Node}
+  nnodes::Int64
+end 
+function Tree(value)
+  return Tree(Dict(zip([1],[Node(missing,Vector{Int64}(),0,value,[1])])),1)
+end 
+function root(T::Tree)
+  return T.nodes[1]
+end
+function pushchild!(tree::Tree, parentid::Int , value::Any)
+  1 <= parentid <= tree.nnodes || throw(BoundsError(tree,parentid))
+  tree.nnodes += 1
+  push!(tree.nodes[parentid].children,tree.nnodes) # add child id to parents child array
+  tree.nodes[parentid].nchildren+=1     # add child counter
+  push!(tree.nodes,(tree.nnodes => Node(parentid,Vector{Int64}(),0,value,vcat(tree.nodes[parentid].path,tree.nnodes)))) # push new child to the Dictionary & Update path
+  return tree
+end
+function isroot(n::Node)
+  typeof(n.parent) == Missing || return false 
+  return true 
+end 
+function isleaf(n::Node)
+  n.children == Vector{Int64}() || return false
+  return true 
+end 
+function all_root2leaf_paths(T::Tree)
+  P = []
+  for n_i in 1:T.nnodes
+      !isleaf(T.nodes[n_i]) || push!(P,T.nodes[n_i].path) 
+  end 
+  return P
+end
+################################################################################################################################
+function read_path(T::Tree,p,Cyc::Vector{Vector{Int64}},Info=0) # where p::path is the path along we want to generate blocksys, Cyc is the array of disjoint Cycles, Info is some extra TODO to avoid generating wrong pps.
+  # return a List of all possible blocksys corresponding to this path
+  #= 
+    TODO implement this function as iterator for new type (just go along the tree and "twistcombine" from state to state)
+  =#
+  L = []
+  for node in p 
+    node != 1 || continue
+    println(node)
+    nv = T.nodes[node].value   # (d,k) => List of used cyc
+    key = nv[1]
+    Used_cyc = nv[2]
+    Used_elem = []
+    for used_cyc in Used_cyc
+      Used_elem = union(Used_elem,Cyc[used_cyc])
+    end 
+      #= 
+        WARNING here is still a bug, (we only merge some bs incorrectly)
+        Hier noch Nach cyclen in Cyc trennen und (d,k) explizit verwenden!!
+      =#
+    # append results to the last of elements in L (if L = [] generate new)
+    if L == []
+      for bs in Hecke.subsets(Used_elem,divexact(key[1],key[2]))    # pick d / k Elements from used cycleset
+        push!(L,bs)
+      end
+    else 
+      # append on each element each new bs (copy and push) 
+
+      D = []   # temporary naive slow solutione here
+      for bs in Hecke.subsets(Used_elem,divexact(key[1],key[2]))    # pick d / k Elements from used cycleset
+        for bsl in L 
+          push!(D,vcat(bsl,bs))
+        end 
+      end
+      L = deepcopy(D)
+    end 
+  end 
+  return L 
+end 
+function all_subsets(value,k,D,min,checkset)# naive brute force variant. suff. for subsets in Klüners algorithm. where D Dictionary with A -> n_a 
   #=
-   TODO later: combinatorical optimization here.
-   this is kind of partition problem 
-   finding all cycles that gives a block combined with k
-   keyset for 
+   TODO later: combinatorical optimization here. This is kind of partition problem finding all cycles that gives a block combined with k of blocksize d
   =#
   keyset = Set() # keyset ( k | n_a)
   for key in keys(D)
@@ -138,7 +217,7 @@ function all_subsets(value,k,D,min,checkset)
     if isempty(set)
       continue
     end
-    if !(min in set)  # fixed one in rekursion
+    if !(min in set)  # fixed one in recursion
       continue
     end
     if value == sum(D[i] for i in set )
@@ -147,8 +226,33 @@ function all_subsets(value,k,D,min,checkset)
   end 
   return L 
 end 
-
-function all_blocksys_naive(C)
+function recursion!(T,n,N_i,set,compset,deep)
+  L = []
+  v = minimum(set) # fix value on recursion start (v = 1) # contained in first block
+  for d in divisors(n)
+    for k in divisors(d)
+      value = d*k
+      S_A = all_subsets(value,k,N_i,v,set)#return Cyc
+      for generated_set in S_A
+        temp = [[d,k],collect(generated_set)]
+        println("push:")
+        T = pushchild!(T,deep,([d,k] => collect(generated_set))) 
+        compset2 = generated_set
+        set2 = setdiff(set,compset2)
+        if !isempty(set2)
+          println("recursion:")
+          S_R,T = recursion!(T,n,N_i,set2,compset2,deep+1)  # TODO later only T here , we can manage the content of L with parents of nodes of T
+          temp = push!(temp,collect(S_R))
+        end
+        L = push!(L,temp)
+      end
+      if d == n# pick d/k elementes from the cycle and return it
+        return L,T # TODO later only T here
+      end
+    end
+  end
+end
+function all_blocksys_naive(C::Oscar.GaloisGrp.GaloisCtx{Hecke.qAdicRootCtx}) # change signature and function name (after i fixed my new BS type)
   Rts = roots(C,7)
   cyc = gap_perm([findfirst(y -> y == x, Rts) for x = map(frobenius,Rts)])
   n = degree(C.f)
@@ -160,112 +264,12 @@ function all_blocksys_naive(C)
   N_i = Dict(zip([i for i in 1:l],length.(Cyc)))
   set = Set([i for i in 1:l])
   compset = Set()
-  return recursion(n,N_i,set,compset)
+  T = Tree(0)
+  recursion!(T,n,N_i,set,compset,1)
+  return T 
+  # returns the blocksys structure Tree 
 end
-
-function recursion(n,N_i,set,compset)
-  L = []
-  v = minimum(set) # fix value on recursion start (v = 1) # contained in first block
-  for d in divisors(n)
-    for k in divisors(d)
-      value = d*k
-      S_A = all_subsets(value,k,N_i,v,set)#return Cyc
-      println("d =  $d und k = $k")# return s.t dk = sum n_i
-      println(S_A)
-      for generated_set in S_A
-        temp = [[d,k],collect(generated_set)]
-        compset2 = generated_set
-        set2 = setdiff(set,compset2)
-        if !isempty(set2)
-          S_R = recursion(n,N_i,set2,compset2)
-          println("recursion:")
-          println(set2)
-          temp = push!(temp,collect(S_R))
-        end
-        L = push!(L,temp)
-      end
-      if d == n# pick d/k elementes from the cycle and return it
-        return L
-        # aktuell L ist der Bauplan für die möglichen Blocksysteme, die wir aus Cyc zusammensetzen können.
-        # Hier wird dann jede mögliche Wahl der (d,k) Teilmengen von L_i von einem cycel iteriert.
-        #return generate_blocksys(L,Cyc) 
-        #TODO 
-      end
-    end
-  end
-end
-function generate_blocksys(L,T) 
-  #where A is a List of Subset of Powerset of { 1 .... l } for init-composing blocks with (d,k) entries.
-  # T is the cycle decomposition
-  B = []
-  for array in L 
-    B = vcat!(B,push_array(array,T))
-  end 
-  return B 
-end 
 
 #Also good idea:
 #
-# find all pos. blocks:
-# bin. Search tree T, 
-# update Orbits (of perm.)
-# iter through T (avoids doubles orbit calc.)
-# 
-#
-
-
-################################################################################################################################
-# basic tree with unlimited children per node, inpush updated paths to all leaves
-# ids are updated during puhses, nodes are stored by id in a Dict
-# acces via id. 
-################################################################################################################################
-mutable struct Node
-  parent::Union{Int64,Missing}
-  childs::Vector{Int64}
-  nchilds::Int64
-  value::Any   #later d,n and Vector here
-  path::Vector{Int64}
-end
-mutable struct Tree  
-  nodes::Dict{Int64, Node}
-  nnodes::Int64
-end 
-## basic stuff 
-Tree(value) = Tree(Dict(zip([1],[Node(missing,Vector{Int64}(),0,value,[1])])),1)
-root(T::Tree) = T.nodes[1]
-function pushchild!(tree::Tree, parentid::Int , value::Any)
-  1 <= parentid <= tree.nnodes || throw(BoundsError(tree, parentid))
-  tree.nnodes += 1
-  push!(tree.nodes[parentid].childs,tree.nnodes) # add child id to parents child array
-  tree.nodes[parentid].nchilds+=1     # add child counter
-  #push!(tree.nodes,(tree.nnodes => Node( parentid, Vector{Int64}(),0,value))) # push new child to the Dictionary
-  push!(tree.nodes,(tree.nnodes => Node( parentid, Vector{Int64}(),0,value,vcat(T.nodes[parentid].path,tree.nnodes)))) # push new child to the Dictionary
-
-end
-function isroot(n::Node)
-  typeof(n.parent) == Missing || return false 
-  return true 
-end 
-function isleaf(n::Node)
-  n.childs == Vector{Int64}() || return false
-  return true 
-end 
-function all_root2leaf_paths(T::Tree)
-  P = []
-  for n_i in 1:T.nnodes
-      !isleaf(T.nodes[n_i]) || push!(P,T.nodes[n_i].path) 
-  end 
-  return P
-end
-################################################################################################################################
-### we want to give an iterator that iterates all paths from root to any leaf 
-# output array with nodeid's for paths
-# TODO iterate over leafs in preorder !
-#=
-all_root2leaf_paths(T)
-T = Tree(123)
-pushchild!(T,5,1121233123)
-T.nodes
-firstpath(T)
-isleaf(T.nodes[1])
-=#
+# find all pos. blocks cont 1: update in a BST (by Orbits of perm.) During iter we can avoid doublecheck and recalc orbits due to the tree struct.
