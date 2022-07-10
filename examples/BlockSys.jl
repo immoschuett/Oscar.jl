7b6b7ea3aa1c7f7cc003dba6ef932902da962197
Immo <immo.schuett@outlook.de>
2022-07-10 23:53:19 +0200

    some tests

--- examples/BlockSys.jl
+++ examples/BlockSys.jl
@@ -120,6 +120,7 @@ end
 # basic tree with unlimited children per node, inpush updated paths to all leaves
 # ids are updated during puhses, nodes are stored by id in a Dict
 # acces via node-id. 
+# update leafs in the end #TODO improve this feature inpush
 ################################################################################################################################
 mutable struct Node
   parent::Union{Int64,Missing}
@@ -129,11 +130,18 @@ mutable struct Node
   path::Vector{Int64}  #<- TODO new type path with attributes length and nodeids etc...
 end
 mutable struct Tree  
-  nodes::Dict{Int64, Node}
+  nodes::Dict{Int64, Node} # for performance reasons maybe Array here (unnecessary to call hash function on our practical size)
   nnodes::Int64
+  leafs::Vector{Int64}
+end 
+mutable struct compressed_Blocksystem
+  T::Tree # dataflowtree with k and used Cyc
+  d::Int64 # blocksize
+  Cyc::Vector{Vector{Int64}} # disjoint cycles
+  N::Vector{Int64}
 end 
 function Tree(value)
-  return Tree(Dict(zip([1],[Node(missing,Vector{Int64}(),0,value,[1])])),1)
+  return Tree(Dict(zip([1],[Node(missing,Vector{Int64}(),0,value,[1])])),1,Vector{Int64}())
 end 
 function root(T::Tree)
   return T.nodes[1]
@@ -154,6 +162,13 @@ function isleaf(n::Node)
   n.children == Vector{Int64}() || return false
   return true 
 end 
+function leafs(T::Tree)
+  leafs = []
+  for n_i in 1:T.nnodes
+    !isleaf(T.nodes[n_i]) || push!(leafs,n_i) 
+  end 
+  return leafs
+end
 function all_root2leaf_paths(T::Tree)
   P = []
   for n_i in 1:T.nnodes
@@ -162,114 +177,144 @@ function all_root2leaf_paths(T::Tree)
   return P
 end
 ################################################################################################################################
-function read_path(T::Tree,p,Cyc::Vector{Vector{Int64}},Info=0) # where p::path is the path along we want to generate blocksys, Cyc is the array of disjoint Cycles, Info is some extra TODO to avoid generating wrong pps.
-  # return a List of all possible blocksys corresponding to this path
-  #= 
-    TODO implement this function as iterator for new type (just go along the tree and "twistcombine" from state to state)
-  =#
-  L = []
-  for node in p 
-    node != 1 || continue
-    println(node)
-    nv = T.nodes[node].value   # (d,k) => List of used cyc
-    key = nv[1]
-    Used_cyc = nv[2]
-    Used_elem = []
-    for used_cyc in Used_cyc
-      Used_elem = union(Used_elem,Cyc[used_cyc])
-    end 
-      #= 
-        WARNING here is still a bug, (we only merge some bs incorrectly)
-        Hier noch Nach cyclen in Cyc trennen und (d,k) explizit verwenden!!
-      =#
-    # append results to the last of elements in L (if L = [] generate new)
-    if L == []
-      for bs in Hecke.subsets(Used_elem,divexact(key[1],key[2]))    # pick d / k Elements from used cycleset
-        push!(L,bs)
-      end
-    else 
-      # append on each element each new bs (copy and push) 
-
-      D = []   # temporary naive slow solutione here
-      for bs in Hecke.subsets(Used_elem,divexact(key[1],key[2]))    # pick d / k Elements from used cycleset
-        for bsl in L 
-          push!(D,vcat(bsl,bs))
-        end 
-      end
-      L = deepcopy(D)
-    end 
-  end 
-  return L 
-end 
-function all_subsets(value,k,D,min,checkset)# naive brute force variant. suff. for subsets in Klüners algorithm. where D Dictionary with A -> n_a 
-  #=
-   TODO later: combinatorical optimization here. This is kind of partition problem finding all cycles that gives a block combined with k of blocksize d
-  =#
-  keyset = Set() # keyset ( k | n_a)
-  for key in keys(D)
-    if D[key]%k == 0 && key in checkset
-      push!(keyset,key)
-    end
-  end 
-  S = Hecke.subsets(keyset)
-  L = []
-  for set in S 
-    if isempty(set)
-      continue
-    end
-    if !(min in set)  # fixed one in recursion
-      continue
-    end
-    if value == sum(D[i] for i in set )
-      push!(L,collect(set))
-    end 
-  end 
-  return L 
-end 
-function recursion!(T,n,N_i,set,compset,deep)
-  L = []
-  v = minimum(set) # fix value on recursion start (v = 1) # contained in first block
-  for d in divisors(n)
-    for k in divisors(d)
-      value = d*k
-      S_A = all_subsets(value,k,N_i,v,set)#return Cyc
-      for generated_set in S_A
-        temp = [[d,k],collect(generated_set)]
-        println("push:")
-        T = pushchild!(T,deep,([d,k] => collect(generated_set))) 
-        compset2 = generated_set
-        set2 = setdiff(set,compset2)
-        if !isempty(set2)
-          println("recursion:")
-          S_R,T = recursion!(T,n,N_i,set2,compset2,deep+1)  # TODO later only T here , we can manage the content of L with parents of nodes of T
-          temp = push!(temp,collect(S_R))
-        end
-        L = push!(L,temp)
-      end
-      if d == n# pick d/k elementes from the cycle and return it
-        return L,T # TODO later only T here
-      end
-    end
-  end
-end
-function all_blocksys_naive(C::Oscar.GaloisGrp.GaloisCtx{Hecke.qAdicRootCtx}) # change signature and function name (after i fixed my new BS type)
-  Rts = roots(C,7)
-  cyc = gap_perm([findfirst(y -> y == x, Rts) for x = map(frobenius,Rts)])
-  n = degree(C.f)
-  Cyc = map(collect,orbits(gset(sub(symmetric_group(n),[cyc])[1]))) 
-  n_1 = length(Cyc[1])
-  l = length(Cyc)
-  A = Set{Int64}(2:l)
-  Cyc_values = Dict(zip([i for i in 2:l],Cyc[2:l]))
-  N_i = Dict(zip([i for i in 1:l],length.(Cyc)))
-  set = Set([i for i in 1:l])
-  compset = Set()
-  T = Tree(0)
-  recursion!(T,n,N_i,set,compset,1)
-  return T 
-  # returns the blocksys structure Tree 
-end
 
 #Also good idea:
 #
 # find all pos. blocks cont 1: update in a BST (by Orbits of perm.) During iter we can avoid doublecheck and recalc orbits due to the tree struct.
+#TODO 
+# Runtime tests
+# Pseudotests & new Date Structure 
+# 
+function block_klueners(C::Oscar.GaloisGrp.GaloisCtx{Hecke.qAdicRootCtx},d) # change signature and function name (after i fixed my new BS type)
+  # if d = 1 manualle or else k= 1 = d and dk - n1 < 0 possible
+  Rts = roots(C,7)
+  cyc = gap_perm([findfirst(y -> y == x, Rts) for x = map(frobenius,Rts)])
+  n = degree(C.f)
+  divexact(n,d) # catch errors fast
+  Cyc = map(collect,orbits(gset(sub(symmetric_group(n),[cyc])[1]))) 
+  l = length(Cyc)
+  Z_values = Dict(zip([i for i in 1:l],length.(Cyc)))
+  T = Tree(0)
+  O = kdivisors(vcat(length.(Cyc),[d]))
+  #O = divisors(d)
+  A = Set([i for i=1:l])
+  T = rec_klueners(T,d,A,Z_values,cyc,1,O)
+  T.leafs =  leafs(T)
+  return compressed_Blocksystem(T,d,Cyc,length.(Cyc))
+  # returns the blocksys structure Tree 
+end
+function kdivisors(U)
+  O = Set()
+  for g in U # experimental here
+    O = union(O,Set(divisors(g)))
+  end 
+  return O
+end 
+function rec_klueners(T,d,A,Z_values,cyc,parentid,g) 
+  l = length(Z_values)#
+  for k in g   # note there exists k_i for each conjugated block 
+    for set in specialsets(A,Z_values,k*d,k) # find all subsets. st. dk-minA = sum others
+      pushchild!(T,parentid,(k => set))
+      A_new = setdiff(A,set)
+      isempty(A_new) ||  rec_klueners(T,d,A_new,Z_values,cyc,T.nnodes,g) # append to last pushed child (with id T.nnodes)
+    end 
+  end
+  return T 
+end 
+function specialsets(A,Zv,v,k)
+  #return subsets A s.t. kd - min(A) = Sum n_a    , k | n_a inkl min.
+  if isempty(A)
+    return []
+  end 
+  m = minimum(A)
+  M = Set([m])
+  A_new = setdiff(A,M)
+  B = Set()   
+  for i in A_new
+    if Zv[i]%k == 0 
+      push!(B,i)
+    end 
+  end   # dies kann man noch optimieren (readout Z_valuse.%k == 0 und erstelle A )
+  L = []
+  for C in Hecke.subsets(B)
+    D = union(C,M)
+    #if !(min in A)  # fixed one in recursion
+    #  continue
+    #end
+    if v == sum([Zv[i] for i in D])
+      push!(L,D)
+    end 
+  end 
+  return L
+end 
+
+
+
+function read_path_to_leaf(CB::compressed_Blocksystem,id)# where id is the leaf id
+  #return all blocksystems along the unique path to the leaf with given id
+  d = CB.d
+  #CB.Cyc
+  B = []
+  for ids in CB.T.nodes[id].path[2:end] # path without the root
+    push!(B,all_choice_blocks(CB,ids))
+  end 
+  BL = [[b] for b in B[1]]
+  BL2 = []
+  for i = 2:length(B)
+    for bl in BL
+      for bli in B[i]
+        push!(BL2,vcat(bl,[bli])
+      end 
+    end 
+    BL = BL2
+  end 
+  return BL 
+end 
+
+
+function all_choice_blocks(CB,ids)
+  # Cyc = CB.Cyc
+  # k,usedC = CB.T.nodes[ids].values
+  # for used_cycle in CB.T.nodes[ids].values[2]
+  n_blocks = length(CB.T.nodes[ids].values[2]) # je n/k aus jedem block 
+  # TODO
+  E = []
+  for j = 1:length(CB.T.nodes[ids].values[2])
+    choose = divexavt(CB.T.nodes[ids].values[2][k],CB.T.nodes[ids].values[1]) # na_ / k , how many elements from each cycle we can choose to get a valid block
+    if j == 1 
+      for set in Hecke.subsets(CB.Cyc[j]),choose)
+        push!(E,set)
+      end 
+    end 
+  end 
+# TODO
+end 
+
+function timetest(TESTFIELDS)
+  #where Testfields is a List of defining polynomial
+  Zx, x = ZZ["x"];
+  for polynom in TESTFIELDS
+    C = Oscar.GaloisGrp.GaloisCtx(polynom[2], 17);
+    K, _ = number_field(C.f);
+    S = Oscar.GaloisGrp.SubfieldLattice(K, C);
+    L = collect_subfields(C,false)
+  end 
+end 
+function timetest2(ll)
+  for b in ll
+    g = galois_group(b[2])[1];
+    #tg = transitive_identification(g);
+    #si = signature(b[2])[1];
+    #fil = open("bal$(tg)st$(si)", "a")
+    #println(fil, "[",b[1],",",b[2],"],")
+    #close(fil)
+  end 
+end
+
+#=
+ll = [eval(Meta.parse(line[1:length(line)-1])) for line in split(str)]; # where str = [ ... DATA ]
+julia> @time   timetest(ll)
+  1.094483 seconds (5.05 M allocations: 283.014 MiB, 9.15% gc time)
+julia> @time   timetest2(ll)
+  5.179882 seconds (21.97 M allocations: 1.208 GiB, 6.59% gc time)
+=#
